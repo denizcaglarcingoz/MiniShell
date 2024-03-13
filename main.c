d680db456b264cd9ce7cab2d3dae7d313236d0b4
denizcaglarcingoz <dcingoz@c1r1p1.42vienna.com>
2024-03-13 20:30:59 +0100

    First Prompt

--- main.c
+++ main.c
@@ -3,9 +3,25 @@
 #include <readline/history.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <signal.h>
+#include <termcap.h>
+#include <ncurses.h>
+#include <unistd.h>
+#include <sys/ioctl.h> 
 
-int main() {
+void sigint_handler(int signum)
+{
+	ioctl(STDIN_FILENO, TIOCSTI, "\n");
+	rl_on_new_line();
+	rl_replace_line("", 0);
+}
+
+int main()
+{
     
+
+	signal(SIGINT, sigint_handler);
+
 	while (1)
 	{
         char* input = readline("MINISHELL> ");
@@ -20,14 +36,15 @@ int main() {
 			clear_history();
 			break;
 		}
-        add_history(input);
+        if(strcmp(input, "") != 0)
+		{
+			add_history(input);
+		}
         if (strcmp(input, "exit") == 0) {
             printf("Exiting MINISHELL!\n");
             free(input);
             break;
         }
-
-        printf("You entered: %s\n", input);
     }
 
     return 0;
