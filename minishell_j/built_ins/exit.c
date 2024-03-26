2b2d5f7dc30e665c4bbe2d5d2dafa86e728580ec
Jesse Daniel Hotchkin <jhotchki@c2r3p1.42vienna.com>
2024-03-26 14:41:49 +0100

    delete

--- minishell_j/built_ins/exit.c
+++
@@ -1,9 +0,0 @@
-#include "../minishell.h"
-
-void	ft_exit(t_tokens *tokens)
-{
-    ft_putstr_color_fd(1, "Exiting MINISHELL!\n", BOLD_GREEN);
-	clear_history();
-	free_list(&tokens);
-	exit(EXIT_SUCCESS);
-}
