f26605ae2d92e61cee64bc4f413ee5fa4f59310c
Deniz Caglar Cingoz <dcingoz@c2r2p2.42vienna.com>
2024-07-07 22:13:33 +0200

    Pipe hdoc in expansion

--- src/backend/execution/pipe_hdoc.c
+++ src/backend/execution/pipe_hdoc.c
@@ -6,7 +6,7 @@
 /*   By: dcingoz <dcingoz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 01:06:33 by dcingoz           #+#    #+#             */
-/*   Updated: 2024/07/05 01:10:35 by dcingoz          ###   ########.fr       */
+/*   Updated: 2024/07/07 22:11:35 by dcingoz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,8 @@ int	is_hdoc_exist(t_shell *shell)
 
 int	pipe_hdocs(t_shell *shell)
 {
-	int	i;
+	int		i;
+	char	*non_exp_hdoc;
 
 	if (is_hdoc_exist(shell) == 0)
 		return (0);
@@ -45,10 +46,14 @@ int	pipe_hdocs(t_shell *shell)
 	{
 		if (expandor_hdoc(shell, i) == false)
 			return (1);
+		non_exp_hdoc = get_last_non_exp_hdoc(shell->tables[i], shell);
 		if (shell->tables[i].heredoc[0] == NULL)
 			shell->hdoc[i] = ft_strdup("");
 		else
+		{	
 			shell->hdoc[i] = check_hdoc_p(shell->tables[i], shell);
+			shell->hdoc[i] = hdoc_in_expand(shell, shell->hdoc[i], non_exp_hdoc);
+		}
 		if (g_sig_int == SIGUSR1)
 			return (free_all(shell, "", 0), 1);
 		i++;
